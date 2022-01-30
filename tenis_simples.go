1bcddc698378ec20b79806ea38718787b3dae2e5
Marcelo Moura <marcelo.moura1511@gmail.com>
2022-01-30 19:58:59 -0300

    permitindo configurar o número de sets, games e pontos em cada game

--- tenis_simples.go
+++ tenis_simples.go
@@ -9,18 +9,23 @@ import (
 	"sync"
 )
 
-var qtdPontos = 0
+var qtdPontosGame = 0
+var qtdGames = 0
+var qtdSets = 0
 var players = [2]string{"Jogador 1", "Jogador 2"}
 var pontuacao = [2]int{0,0} 
+var games = [2]int{0,0} 
+var sets = [2]int{0,0} 
 
 var wg sync.WaitGroup
 
 var encerrar = false
 var saque = true
-func partida(jogadorIndex int, games chan int) {
+func partida(jogadorIndex int, passos chan int) {
 	//esperando que as goroutines terminem
 	defer wg.Done();
 	for true {
+		fmt.Print()
 		//verificando se a partida deve ser encerrada
 		if encerrar {
 			fmt.Printf("%s venceu a partida.\n", players[jogadorIndex])
@@ -52,15 +57,30 @@ func partida(jogadorIndex int, games chan int) {
 				fmt.Printf("%s rebateu.\n", players[jogadorIndex])
 			}
 		}
-		game := <-games
-		//verificando se o jogo acabou e fechando o canal
-		if pontuacao[oponenteIndex] >= qtdPontos && pontuacao[oponenteIndex]-pontuacao[jogadorIndex]>1  {
-			close(games)
+		passo := <-passos
+		if pontuacao[oponenteIndex] >= qtdPontosGame && pontuacao[oponenteIndex]-pontuacao[jogadorIndex]>1  {
+			games[oponenteIndex]++
+			gamesjogados := games[jogadorIndex]+games[oponenteIndex]
+			fmt.Printf("%s ganhou o %dº game.\n", players[oponenteIndex], gamesjogados)
+			fmt.Print("\n******** Placar de Games ********\n")
+			fmt.Printf("%s | %d x %d | %s \n\n", players[0], games[0], games[1], players[1])
+			pontuacao = [2]int{0, 0}
+		}
+		if games[oponenteIndex] >= qtdGames && games[oponenteIndex]-games[jogadorIndex]>1  {
+			sets[oponenteIndex]++
+			setjogados:=sets[jogadorIndex]+sets[oponenteIndex]
+			fmt.Printf("%s ganhou o %dº set.\n", players[oponenteIndex], setjogados)
+			fmt.Print("\n******** Placar de Sets ********\n")
+			fmt.Printf("%s | %d x %d | %s \n\n", players[0], sets[0], sets[1], players[1])
+			games = [2]int{0, 0}
+		}
+		if sets[oponenteIndex]==qtdSets{
+			close(passos)
 			encerrar=true
 			break
 		}
-		game++
-		games <- game
+		passo++
+		passos <- passo
 	}
 	// wg.Done()
 }
@@ -93,14 +113,33 @@ func main() {
 	}
 
 	num, erro:= strconv.Atoi(os.Args[1])
+	num1, erro1:= strconv.Atoi(os.Args[2])
+	num2, erro2:= strconv.Atoi(os.Args[3])
 
 	if erro != nil {
 		fmt.Println("Erro, informe um número inteiro para limite de pontos do game.")
 		os.Exit(2)
 	}
-
 	if num < 2{
-		fmt.Println("Erro, informe um número inteiro maior ou igual a 2.")
+		fmt.Println("Erro, informe um número inteiro maior ou igual a 2 para limite de pontos.")
+		os.Exit(2)
+	}
+	if erro1 != nil {
+		fmt.Println("Erro, informe um número inteiro para limite de games.")
+		os.Exit(2)
+	}
+
+	if num1 < 1{
+		fmt.Println("Erro, informe um número inteiro maior ou igual a 1 para limite de games.")
+		os.Exit(2)
+	}
+	if erro2 != nil {
+		fmt.Println("Erro, informe um número inteiro para limite de sets.")
+		os.Exit(2)
+	}
+
+	if num2 < 1{
+		fmt.Println("Erro, informe um número inteiro maior ou igual a 1 para limite de sets.")
 		os.Exit(2)
 	}
 
@@ -108,15 +147,18 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	//qtd de pontos para finalizar a partida
-	qtdPontos = num
+	qtdPontosGame = num
+	qtdGames = num1
+	qtdSets = num2
+	fmt.Printf("%d, %d, %d", qtdPontosGame, qtdGames, qtdSets)
 	//canal do game
-	games := make(chan int)
+	passos := make(chan int)
 
 	wg.Add(2)
 
-	go partida(0, games)
-	games <- 1
-	go partida(1, games)
+	go partida(0, passos)
+	passos <- 1
+	go partida(1, passos)
 
 
 	wg.Wait()
